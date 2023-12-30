305f15a800a0ff5d1ffc9bace4b5e0e6b04852f7
francealip <f.aliprandi@studenti.unipi.it>
2023-12-30 15:59:07 +0100

    fixing log_experiment for holdout

--- src/main/__init__.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-
-PROJECT_ROOT = Path(__file__).parent.parent.parent.resolve()
-DATA_ROOT = PROJECT_ROOT / "data_for_testing"
-HPARAMS_ROOT = PROJECT_ROOT / "hyperparams"
